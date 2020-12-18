9a3bc20e181f8d7ca3d8767a100b3532f0c38109
oguzhanoxel <oguzhanoksel@hotmail.com>
2020-12-18 16:49:41 +0300

    added product reviews

--- apps/store/views.py
+++ apps/store/views.py
@@ -2,7 +2,7 @@ import random
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
 
-from .models import Product, Category
+from .models import Product, Category, ProductReview
 
 from apps.cart.cart import Cart
 
@@ -21,6 +21,16 @@ def search(request):
 
 def product_detail(request, category_slug, slug):
     product = get_object_or_404(Product, slug=slug)
+
+    # Add review
+    if request.method == 'POST' and request.user.is_authenticated:
+        stars = request.POST.get('stars', 3)
+        content = request.POST.get('content', '')
+
+        review = ProductReview.objects.create(product=product, user=request.user, stars=stars, content=content)
+
+        return redirect('product_detail', category_slug=category_slug, slug=slug)
+    # 
 
     related_products = list(product.category.products.filter(parent=None).exclude(id=product.id))
     if len(related_products) >= 3:
