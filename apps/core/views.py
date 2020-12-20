c07fc07f2f2b9cb55992b35ac57addc415bc87b3
oguzhanoxel <oguzhanoksel@hotmail.com>
2020-12-20 12:01:52 +0300

    featured categories

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from apps.store.models import Product
+from apps.store.models import Product, Category
 # from apps.order.models import Order
 
 
@@ -11,8 +11,11 @@ from apps.store.models import Product
 
 def frontpage(request):
     products = Product.objects.filter(is_featured=True)
+    featured_categories = Category.objects.filter(is_featured=True)
+
     context = {
         'products': products,
+        'featured_categories': featured_categories,
     }
     return render(request, 'frontpage.html', context)
 
